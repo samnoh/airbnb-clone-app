441cc86a93bf6e290c3e0bc89f72df4769a83f1d
Sam Noh <samnoh21@outlook.com>
2019-12-29 18:54:54 +1300

    refactor(rooms): WriteRoomSerializer

--- users/views.py
+++ users/views.py
@@ -1,18 +1,25 @@
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from .models import User
-from .serializers import ReadUserSerializer
+from .serializers import ReadUserSerializer, WriteUserSerializer
 
 
 class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
     def get(self, request):
-        if request.user.is_authenticated:
-            return Response(ReadUserSerializer(request.user).data)
+        return Response(ReadUserSerializer(request.user).data)
 
     def put(self, request):
-        pass
+        serializer = WriteUserSerializer(request.user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response()
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
 
 
 @api_view(["GET"])
