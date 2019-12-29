84748f52433adde5ebde1f32db987b3736ebf5b9
Sam Noh <samnoh21@outlook.com>
2019-12-29 18:37:56 +1300

    feat(users): update admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
+
+
+class PhotoInline(admin.TabularInline):
+
+    model = models.Photo
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+
+    inlines = (PhotoInline,)
 
     list_display = (
         "name",
@@ -13,4 +21,9 @@ class RoomAdmin(admin.ModelAdmin):
 
 @admin.register(models.Photo)
 class PhotoAdmin(admin.ModelAdmin):
-    pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        return mark_safe(f'<img width=50 src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
