a18a13e1cad499c70da8bb5b09a9cf00f1faaf43
Sam Noh <samnoh21@outlook.com>
2019-12-30 20:35:20 +1300

    chore(): clean up

--- users/views.py
+++ users/views.py
@@ -6,7 +6,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
-from rooms.serializers import RoomSerializer
 from rooms.models import Room
 from .models import User
 from .serializers import UserSerializer
