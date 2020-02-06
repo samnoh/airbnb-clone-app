e07a4b022e6f0dff0f3cf1cf0160334efdec6de9
Sam Noh <samnoh21@outlook.com>
2020-02-06 22:52:09 +1300

    chore(config): update default renderer on prod

--- config/settings.py
+++ config/settings.py
@@ -150,3 +150,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
     ],
 }
+
+if DEBUG:
+    REST_FRAMEWORK["DEFAULT_RENDERER_CLASSES"] = [
+        "rest_framework.renderers.JSONRenderer",
+    ]
